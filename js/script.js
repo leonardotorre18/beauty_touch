feb79d96cda10876871eab5aa9896f41af1859a4
Leonardotorre18 <leonardotorre14@gmail.com>
2022-03-04 22:29:32 -0400

    Added Menu Burger in navigation menu Navigation Menu is responsive now Added about-us Page

--- js/script.js
+++ js/script.js
@@ -1,24 +1,10 @@
-// Call Plugin Owl Carousel 2
-$(document).ready(function() {
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        autoplay: true,
-        items: 1,
-        autoplayTimeout: 3000,
-        margin: 10,
-        nav: true,
-        navText: ['Anterior', 'Siguiente'],
-        responsive: {
-            1200: {
-                items: 4
-            },
-            992: {
-                items: 3
-            },
-            700: {
-                items: 2
-            }
-        }
+"use strict";
 
-    });
+$(document).ready(function() {
+
+    // Menu burger - Button Toggle
+    $('#menu-burger').click(() => {
+        $('#menu').toggleClass('show');
+    })
+
 });
