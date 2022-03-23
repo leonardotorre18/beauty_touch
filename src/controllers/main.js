9736ca68797bb04eba3be1bc9a25bd48f303f895
Leonardotorre18 <leonardotorre14@gmail.com>
2022-03-22 22:43:39 -0400

    Added Mixis file in SCSS from buttons and cards Added All services in JSON Format from engine views Added new variables in SCSS Now erorr 404 view have styles Added Footer in Main Layout and styles with SCSS Created social media icons from footer page Modificated styles in sections page about titles and params
    Corrected Error in Controller from route '/ServicesInfo' when get params incorrects (BackEnd)

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -14,15 +14,23 @@ controller.services = (req, res) => {
     });
 }
 controller.servicesInfo = (req, res) => {
+    const id = parseInt(req.params.id);
+
+    if (id == NaN) res.redirect('/services');
 
     fs.readFile(path.join(__dirname, '../json/services.info.json'), 'utf8', (e, data) => {
         data = JSON.parse(data);
-        for (const key of data) {
-            if (key.id == req.params.id) {
-                res.render('servicesInfo', { data: key });
-                break
+
+        if (id >= data.length) res.redirect('/services');
+        else {
+            for (const key of data) {
+                if (key.id == id) {
+                    res.render('servicesInfo', { data: key });
+                    break
+                }
             }
         }
+
     });
 }
 controller.about = (req, res) => {
