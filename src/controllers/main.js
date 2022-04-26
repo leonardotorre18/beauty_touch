ef98a524a661330391b816bd68d01b783ee5d041
Leonardotorre18 <leonardotorre14@gmail.com>
2022-04-26 00:49:52 -0400

    Delete footer and added Social Bar Created Structure HTML in main.hbs view and Created SCSS Styles file from Social Bar Implemented Condicional Structure handlebars from Jumbotron and OwlCarousel Script Removed tags main in views, now there is one tag main in a main layout

--- src/controllers/main.js
+++ src/controllers/main.js
@@ -1,7 +1,7 @@
 const controller = {}
 
 controller.home = (req, res) => {
-    res.render('home')
+    res.render('home', { jumbotron: true, owlcarousel: true})
 }
 
 controller.about = (req, res) => {
@@ -11,7 +11,7 @@ controller.personal = (req, res) => {
     res.render('about_personal')
 }
 controller.central = (req, res) => {
-    res.render('about_central')
+    res.render('about_central', { owlcarousel: true })
 }
 controller.error404 = (req, res) => {
     res.render('404')
